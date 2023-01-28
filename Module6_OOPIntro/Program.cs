1a1d2e9f5d32ad40289f260fad74a273e2d8a153
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-28 21:31:46 +0300

    Задание 6.2.1: Обьявление класса

--- Module6_OOPIntro/Program.cs
+++ Module6_OOPIntro/Program.cs
@@ -2,11 +2,20 @@
 
 namespace Module6_OOPIntro
 {
+    class Example
+    {
+        public string Name;
+
+        public void Show()
+        {
+
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
