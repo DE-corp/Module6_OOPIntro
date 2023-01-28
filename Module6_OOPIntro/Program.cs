ecf306fcb6bcd154840f912d4c2c72e09c118968
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-28 21:25:08 +0300

    Добавьте файлы проекта.

---
+++ Module6_OOPIntro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module6_OOPIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
