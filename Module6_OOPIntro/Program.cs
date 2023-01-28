885cd5d17afaa5d8f06495d7f9f9c70383f5a824
Evdokimov Dmitry <crazygoup777@gmail.com>
2023-01-28 22:39:27 +0300

    Задание 6.2.8: Создал класс Rectangle для представления прямоугольников и метод в нем, возвращающий площадь прямоугольника

--- Module6_OOPIntro/Program.cs
+++ Module6_OOPIntro/Program.cs
@@ -2,58 +2,39 @@
 
 namespace Module6_OOPIntro
 {
-	class Human
-	{
-		// Поля класса
-		public string name;
-		public int age;
+	class Rectangle
+    {
+		public int a;
+		public int b;
 
-		// Метод класса
-		public void Greetings()
-		{
-			Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
+		public Rectangle()
+        {
+			a = 6;
+			b = 4;
+        }
+
+		public Rectangle(int a, int b)
+        {
+			this.a = a;
+			this.b = b;
 		}
 
-		// Конструктор 1
-		public Human()
+		public Rectangle(int a)
 		{
-			name = "Неизвестно";
-			age = 20;
+			this.a = a;
+			b = a;
 		}
-		// Конструктор 2
-		public Human(string n)
-		{
-			name = n;
-			age = 20;
-		}
-		// Конструктор 3
-		public Human(string n, int a)
-		{
-			name = n;
-			age = a;
-		}
-	}
 
-	struct Animal
-	{
-		// Поля структуры
-		public string type;
-		public string name;
-		public int age;
+		public double Square() => a * b;
 
-		// Метод структуры
-		public void Info()
-		{
-			Console.WriteLine("Это {0} по кличке {1}, ему {2}", type, name, age);
-		}
 	}
 
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			Animal animal = new Animal {type = "Собака", name = "Вольт", age = 4 };
-			animal.Info();
+			Rectangle rectangle = new Rectangle(2, 3);
+            Console.WriteLine($"Площадь прямоугольника = {rectangle.Square()}");
 
 			Console.ReadLine();
 		}
